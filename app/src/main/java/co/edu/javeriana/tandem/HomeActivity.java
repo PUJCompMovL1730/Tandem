3e2e57813013782d460435329b8533c66f6e28b7
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2017-08-26 22:16:05 -0500

    First commit for Tandem APP, first 4 views

---
+++ app/src/main/java/co/edu/javeriana/tandem/HomeActivity.java
@@ -0,0 +1,47 @@
+package co.edu.javeriana.tandem;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng javeriana = new LatLng(4.626951, -74.064160);
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.mini_icon);
+        MarkerOptions marker = new MarkerOptions().position(javeriana).title("Universidad Javeriana").icon(icon);
+        mMap.addMarker(marker);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(javeriana, 16));
+        }
+    }
+}
