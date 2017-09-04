0c06b5c4db4b0d2bfcab63c88d1c02c28dbf3f69
Alejandro Guayara <ca.guayara93@gmail.com>
2017-09-04 00:07:18 -0500

    Revert "Google Places API added"
    This reverts commit d03b74852e0705411b5b7c2c1f45585c83f6da09.

--- TandemApp/app/src/main/java/co/edu/javeriana/tandem/HomeActivity.java
+++ TandemApp/app/src/main/java/co/edu/javeriana/tandem/HomeActivity.java
@@ -16,9 +16,6 @@ import android.view.ViewStub;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -30,7 +27,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HomeActivity extends BaseNavigationActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
+public class HomeActivity extends BaseNavigationActivity implements OnMapReadyCallback {
 
     static final int GOOGLE_PERMISSION = 12;
     GoogleMap mMap;
@@ -41,8 +38,6 @@ public class HomeActivity extends BaseNavigationActivity implements OnMapReadyCa
 
     ViewStub stub;
     View contentView;
-
-    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +63,6 @@ public class HomeActivity extends BaseNavigationActivity implements OnMapReadyCa
         icon = Utils.getBitmapDescriptor(getBaseContext(), R.drawable.tandem, 120, 59);
         markers = new HashMap<>();
         markers.put("Universidad Javeriana", new MarkerOptions().position(javeriana).title("Universidad Javeriana").snippet("Jaaaa perro").icon(icon));
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Places.GEO_DATA_API).addApi(Places.PLACE_DETECTION_API).enableAutoManage(this, this).build();
     }
 
     @Override
@@ -112,11 +105,5 @@ public class HomeActivity extends BaseNavigationActivity implements OnMapReadyCa
         Snackbar.make(findViewById(android.R.id.content), marker.getTitle(), Snackbar.LENGTH_SHORT).show();
         mMap.addMarker(marker);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place, 16));
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
-    {
-        Toast.makeText(this, "Error al intentar conectarse con Google Places API", Toast.LENGTH_SHORT).show();
     }
 }
