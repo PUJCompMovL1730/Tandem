0455181a74650933caa963867b0bcd8e8908c41f
Sebastián Bobadilla <bobadillasebas@gmail.com>
2017-11-12 19:10:43 -0500

    Added tabs to Travel Activity

--- TandemApp/app/src/main/java/co/edu/javeriana/tandemsquad/tandem/TravelsActivity.java
+++ TandemApp/app/src/main/java/co/edu/javeriana/tandemsquad/tandem/TravelsActivity.java
@@ -4,23 +4,25 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.ListView;
 
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FileDownloadTask;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import co.edu.javeriana.tandemsquad.tandem.adapters.TravelAdapter;
 import co.edu.javeriana.tandemsquad.tandem.firebase.FireBaseAuthentication;
 import co.edu.javeriana.tandemsquad.tandem.firebase.FireBaseStorage;
-import co.edu.javeriana.tandemsquad.tandem.negocio.Marcador;
 import co.edu.javeriana.tandemsquad.tandem.negocio.Recorrido;
 import co.edu.javeriana.tandemsquad.tandem.utilities.Utils;
 
@@ -32,6 +34,10 @@ public class TravelsActivity extends NavigationActivity {
     private ListView travels;
     private List<Recorrido> listTravels;
     private TravelAdapter travelAdapter;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +66,11 @@ public class TravelsActivity extends NavigationActivity {
             }
         };
 
-
+        /**
         Marcador marcador_inicio = new Marcador(new LatLng(4.711000, -74.072094), Marcador.Tipo.INICIO, "Bogota");
         Marcador marcador_fin = new Marcador(new LatLng(4.599730, -74.161994), Marcador.Tipo.INICIO, "Otro lugar");
         travelAdapter.add(new Recorrido(marcador_inicio, marcador_fin, Recorrido.Estado.CASUAL));
+         */
     }
 
     @Override
@@ -72,10 +79,25 @@ public class TravelsActivity extends NavigationActivity {
 
         getSupportActionBar().setTitle(R.string.activity_label_travels);
 
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Historial"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tus recorridos"));
+        tabLayout.addTab(tabLayout.newTab().setText("Global"));
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        pagerAdapter = new TabPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+
+        /**
         travels = (ListView) findViewById(R.id.travels_list_view);
         listTravels = new ArrayList<>();
         travelAdapter = new TravelAdapter(this, listTravels);
         travels.setAdapter(travelAdapter);
+         */
     }
 
     @Override
@@ -96,5 +118,35 @@ public class TravelsActivity extends NavigationActivity {
         Intent mainIntent = new Intent(this, LoginActivity.class);
         mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(mainIntent);
+    }
+
+    public class TabPagerAdapter extends FragmentPagerAdapter {
+        public TabPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    //Historial de recorridos del usuario (recorridos finalizados)
+                    return TravelsHistoryFragment.newInstance();
+                case 1:
+                    //Recorridos que ha publicado el usuario y recorridos a los que sea ha unido
+                    return TravelsOwnFragment.newInstance();
+                case 2:
+                    //Recorridos publicados por otros usuarios
+                    return TravelsGlobalFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
---
+++ TandemApp/app/src/main/java/co/edu/javeriana/tandemsquad/tandem/TravelsHistoryFragment.java
@@ -0,0 +1,52 @@
+package co.edu.javeriana.tandemsquad.tandem;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TravelsHistoryFragment extends Fragment {
+
+  private OnFragmentInteractionListener mListener;
+
+  public TravelsHistoryFragment() {
+    // Required empty public constructor
+  }
+
+  public static TravelsHistoryFragment newInstance() {
+    TravelsHistoryFragment fragment = new TravelsHistoryFragment();
+    //Bundle args = new Bundle();
+    //fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_travels_history, container, false);
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mListener = null;
+  }
+
+  /**
+   * This interface must be implemented by activities that contain this
+   * fragment to allow an interaction in this fragment to be communicated
+   * to the activity and potentially other fragments contained in that
+   * activity.
+   * <p>
+   * See the Android Training lesson <a href=
+   * "http://developer.android.com/training/basics/fragments/communicating.html"
+   * >Communicating with Other Fragments</a> for more information.
+   */
+  public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+  }
+}
